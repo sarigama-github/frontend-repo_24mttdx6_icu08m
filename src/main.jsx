e2d9e3de55127806db90eefb74f5896f87054da6
Flames <vibe-coder@flames.com>
2025-11-18 10:54:17 +0000

    Update project prj_24mttdx6 - 2025-11-18T10:54:17.029Z

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Test from './Test'
+import JPLApp from './JPLApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<JPLApp />} />
+        <Route path="/legacy" element={<App />} />
         <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
